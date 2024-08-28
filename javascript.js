2f1684ff6b383045a01343769e2027e953c1667e
Ali Mohamed <hassanali1910@yahoo.ca>
2024-08-27 23:57:00 -0400

    completed step 4 and almost done 5, have some logic problem where the code goes to the else in playround and doesnt pass any of the if and if else. Debugging , almost finished step 5.

--- javascript.js
+++ javascript.js
@@ -17,9 +17,9 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
-    let rock = "rock"
-    let paper = "paper"
-    let scissors = "scissors"
+    let rock = "rock";
+    let paper = "paper";
+    let scissors = "scissors";
 
     let choice = prompt("Pick rock, paper or scissors: ");
     if (choice.toLowerCase() == rock || choice.toLowerCase() == paper || choice.toLowerCase() == scissors) {
@@ -31,5 +31,62 @@ function getHumanChoice() {
     }
 }
 
-console.log(getComputerChoice());
-console.log(getHumanChoice())
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+
+
+    if (humanChoice == "rock" && computerChoice.toLowerCase() == "paper") {
+
+        computerScore += 1;
+        return "You lose! Paper beats Rock";
+    }
+
+    else if (humanChoice == "rock" && computerChoice.toLowerCase() == "scissors") {
+
+        humanScore += 1;
+        return "You win! Rock beats Scissors";
+    }
+
+    else if (humanChoice == "paper" && computerChoice.toLowerCase() == "rock") {
+
+        humanScore += 1;
+        return "You win! Paper beats Rock";
+    }
+
+    else if (humanChoice == "paper" && computerChoice.toLowerCase() == "scissors") {
+
+        computerScore += 1;
+        return "You lose! Scissors beats Paper";
+    }
+
+    else if (humanChoice == "scissors" && computerChoice.toLowerCase() == "paper") {
+
+        humanScore += 1;
+        return "You win! Scissors beats Paper";
+    }
+
+    else if (humanChoice == "scissors" && computerChoice.toLowerCase() == "rock") {
+
+        computerScore += 1;
+        return "You lose! Rock beats Scissors";
+    }
+
+
+
+
+
+
+
+
+
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+console.log(humanSelection);
+console.log(computerSelection);
+console.log(playRound(humanSelection, humanSelection));
