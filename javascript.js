42f76352b7f9321be17bca0a58722f4859780ed7
Ali Mohamed <hassanali1910@yahoo.ca>
2024-08-28 01:18:32 -0400

    finished the rock , paper, scissors project, completed step 6.

--- javascript.js
+++ javascript.js
@@ -32,66 +32,74 @@ function getHumanChoice() {
 }
 
 
-let humanScore = 0;
-let computerScore = 0;
-
-function playRound(humanChoice, computerChoice) {
 
 
-    if (humanChoice == "rock" && computerChoice.toLowerCase() == "paper") {
 
-        computerScore += 1;
-        return "You lose! Paper beats Rock";
+
+function playGame() {
+    let computerScore = 0;
+    let humanScore = 0;
+    function playRound(humanChoice, computerChoice) {
+
+
+        if (humanChoice == "rock" && computerChoice.toLowerCase() == "paper") {
+
+            computerScore += 1;
+            return "You lose! Paper beats Rock";
+        }
+
+        else if (humanChoice == "rock" && computerChoice.toLowerCase() == "scissors") {
+
+            humanScore += 1;
+            return "You win! Rock beats Scissors";
+        }
+
+        else if (humanChoice == "paper" && computerChoice.toLowerCase() == "rock") {
+
+            humanScore += 1;
+            return "You win! Paper beats Rock";
+        }
+
+        else if (humanChoice == "paper" && computerChoice.toLowerCase() == "scissors") {
+
+            computerScore += 1;
+            return "You lose! Scissors beats Paper";
+        }
+
+        else if (humanChoice == "scissors" && computerChoice.toLowerCase() == "paper") {
+
+            humanScore += 1;
+            return "You win! Scissors beats Paper";
+        }
+
+        else if (humanChoice == "scissors" && computerChoice.toLowerCase() == "rock") {
+
+            computerScore += 1;
+            return "You lose! Rock beats Scissors";
+        }
+
+        else {
+            return "Tie! No points to either of you"
+
+        }
+
+
+    }
+    let i = 0;
+
+    for (i = 0; i < 5; i++) {
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+        console.log(playRound(humanSelection, computerSelection));
+
     }
 
-    else if (humanChoice == "rock" && computerChoice.toLowerCase() == "scissors") {
-
-        humanScore += 1;
-        return "You win! Rock beats Scissors";
-    }
-
-    else if (humanChoice == "paper" && computerChoice.toLowerCase() == "rock") {
-
-        humanScore += 1;
-        return "You win! Paper beats Rock";
-    }
-
-    else if (humanChoice == "paper" && computerChoice.toLowerCase() == "scissors") {
-
-        computerScore += 1;
-        return "You lose! Scissors beats Paper";
-    }
-
-    else if (humanChoice == "scissors" && computerChoice.toLowerCase() == "paper") {
-
-        humanScore += 1;
-        return "You win! Scissors beats Paper";
-    }
-
-    else if (humanChoice == "scissors" && computerChoice.toLowerCase() == "rock") {
-
-        computerScore += 1;
-        return "You lose! Rock beats Scissors";
-    }
-
-    else {
-        return "Tie! No points to either of you"
-
-    }
-
-
-
-
-
-
-
+    console.log("Computer score: " + computerScore);
+    console.log("Human score: " + humanScore);
 
 
 }
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
 
-console.log(humanSelection);
-console.log(computerSelection);
-console.log(playRound(humanSelection, computerSelection));
+
+playGame();
