eb3e01c33f7b3eb228cbe107e96d73e3e6bdf5a4
Ali Mohamed <hassanali1910@yahoo.ca>
2024-08-27 22:38:06 -0400

    created getComputerChoice function that returns randomly rock paper or scissors. Step 2 complete"

---
+++ javascript.js
@@ -0,0 +1,19 @@
+function getComputerChoice() {
+
+    let result = Math.floor(Math.random() * 3);
+
+    if (result == 0) {
+        return "Rock"
+    }
+    else if (result == 1) {
+        return "Paper"
+    }
+
+    else {
+        return "Scissors"
+    }
+
+
+}
+
+console.log(getComputerChoice());
